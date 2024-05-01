14bdd28209622f9bb5d1495771e48a76f3e5bb8c
jekamokan <107543919+jekamokan@users.noreply.github.com>
2024-05-02 00:05:25 +0200

    added text-underline-offset

--- vite-project/src/components/AccrodionComponent/index.jsx
+++ vite-project/src/components/AccrodionComponent/index.jsx
@@ -9,7 +9,7 @@ const AccordionComponent = () => {
     return (
         <Accordion type="single" collapsible>
             <AccordionItem value="item-1">
-                <AccordionTrigger>Детальніше</AccordionTrigger>
+                <AccordionTrigger className={"hover:underline-offset-4"} >Детальніше</AccordionTrigger>
                 <AccordionContent>
                     <ul >
                         <li>- Шнурки; регульована за допомогою липучки стрічка</li>
@@ -21,14 +21,14 @@ const AccordionComponent = () => {
                 </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-2">
-                <AccordionTrigger>Опис</AccordionTrigger>
+                <AccordionTrigger className={"hover:underline-offset-4"}>Опис</AccordionTrigger>
                 <AccordionContent>
                     <h4 style={{ fontSize: 30, textTransform: 'uppercase', fontWeight: 'bold', lineHeight: "35px", marginBottom: 10 }}>Низькі кросівки з вібрацією баскетбольної гри.</h4>
                     <p>Не просто взуття, а справжнє заявлення: adidas Forum зробив свій дебют у 1984 році і, після баскетбольного майданчика, завоював музичну сцену. Ця версія повертає стиль 80-х, вібрацію баскетбольної гри та липучкову стрічку класики, упаковуючи всі ці особливості в легку низьку силуетку.</p>
                 </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-3">
-                <AccordionTrigger>Найважливіше</AccordionTrigger>
+                <AccordionTrigger className={"hover:underline-offset-4"}>Найважливіше</AccordionTrigger>
                 <AccordionContent>
                     <p style={{ marginBottom: 20 }}><b>РЕМЕНІ В ОБЛАСТІ КОЛІНА</b> <br />
                         X-подібне нашивання та знімний ремінь на липучці забезпечують додаткову фіксацію і стиль.
